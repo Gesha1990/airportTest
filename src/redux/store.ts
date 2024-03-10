216db9641457dbd3bae7f6bb014662664f50b024
Hennadii Petrov <gesha199019@gmail.com>
2024-03-10 18:27:27 +0200

    fixed brocken app

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,26 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { createLogger } from "redux-logger";
+import { logger } from "redux-logger";
 import { TypedUseSelectorHook, useSelector, useDispatch } from "react-redux";
 import { airportsReducer, routesReducer } from "./slices/index";
 import { RootState, DispatchFunc } from "./interfaces";
-
-const logger = createLogger();
-let middleware: any = [];
-//disable logger on production
-if (process.env.NODE_ENV !== "production") {
-  middleware = (getDefaultMiddleware: Function) =>
-    getDefaultMiddleware().concat(logger);
-} else {
-  middleware = (getDefaultMiddleware: Function) => getDefaultMiddleware();
-}
 
 export const store = configureStore({
   reducer: {
     airports: airportsReducer,
     routes: routesReducer
   },
-  middleware: middleware,
-  devTools: process.env.NODE_ENV !== "production"
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+  devTools: process.env.NODE_ENV === "development"
 });
 
 export const useAppDispatch: DispatchFunc = useDispatch;
