9608e7abd65b15e07b8e307fad65c6eb707d238e
Hennadii Petrov <gesha199019@gmail.com>
2024-03-10 18:05:51 +0200

    disable devtools on prod

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,16 +1,26 @@
 import { configureStore } from "@reduxjs/toolkit";
-import logger from "redux-logger";
+import { createLogger } from "redux-logger";
 import { TypedUseSelectorHook, useSelector, useDispatch } from "react-redux";
 import { airportsReducer, routesReducer } from "./slices/index";
 import { RootState, DispatchFunc } from "./interfaces";
+
+const logger = createLogger();
+let middleware: any = [];
+//disable logger on production
+if (process.env.NODE_ENV !== "production") {
+  middleware = (getDefaultMiddleware: Function) =>
+    getDefaultMiddleware().concat(logger);
+} else {
+  middleware = (getDefaultMiddleware: Function) => getDefaultMiddleware();
+}
 
 export const store = configureStore({
   reducer: {
     airports: airportsReducer,
     routes: routesReducer
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
-  devTools: process.env.NODE_ENV === "development"
+  middleware: middleware,
+  devTools: process.env.NODE_ENV !== "production"
 });
 
 export const useAppDispatch: DispatchFunc = useDispatch;
